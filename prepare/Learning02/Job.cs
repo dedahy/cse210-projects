836f83a5a82eebe5016a68fe0855790a9aa79a25
dedahy <dada.dedahy@gmail.com>
2024-03-06 05:30:54 +0300

    Update

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,26 +1,16 @@
-using System;
+﻿using System;
 
 public class Job
 {
-        public string _jobTitle = "Software Engineer";
-        public string _company = "Microsoft";
-        public int _startYear = 2019;
-        public int _endYear = 2022;
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
 
-        public void display()
-        {
-            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}.");
-        }
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+    
 }
-
-
-//public class resume
-//{
-//    static void Main(string[] args)
-//    {
-//        string _fname = "John";
-//        string _lname = "Smith";
-//        string jobs = "Customer service";
-//    }
-//    
-//}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Customer Service";
+        job1._company = "Webhelp";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+        
+        Resume myresume = new Resume();
+        myresume._name = "Aîcha";
+
+        myresume._jobs.Add(job1);
+        myresume.Display();
     }
+
 }
