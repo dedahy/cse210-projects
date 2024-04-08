9a130a46b4ad661e20260e78bc5a918d14e61cfe
dedahy <dada.dedahy@gmail.com>
2024-04-08 18:11:50 +0300

    week5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,29 +6,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Shape> shapes = new List<Shape>();
-        //Square s = new Square("black", 2);
-        //Rectangle r = new Rectangle("Red", 2, 4);
-//
-        //Console.WriteLine(s.GetArea());
-        //Console.WriteLine(r.GetArea());
 
-        Square s = new Square("black", 2);
-        Rectangle r = new Rectangle("Red", 2, 4);
-        Circle c = new Circle("Yellow", 3);
-
-
-        shapes.Add(s);
-        shapes.Add(r);
-        shapes.Add(c);
-
-        foreach(Shape shape in shapes)
+         static void DisplayWelcome()
         {
-            string color = shape.GetColor();
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
 
-            double area = shape.GetArea();
+            return name;
+        }
 
-            Console.WriteLine($"The {color} shape has an area of {area}.");
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int sqr = number * number;
+            
+            return sqr;
+        }
+        static void DisplayResult(string name, int sqr)
+        {
+            Console.WriteLine($"{name}, the square of your number is {sqr}.");
         }
 
     }
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Diagnostics;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
