dffe3f86b5c2725bf9261a3158d2efb307da887a
dedahy <dada.dedahy@gmail.com>
2024-04-08 18:07:38 +0300

    week5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        //Square s = new Square("black", 2);
+        //Rectangle r = new Rectangle("Red", 2, 4);
+//
+        //Console.WriteLine(s.GetArea());
+        //Console.WriteLine(r.GetArea());
+
+        Square s = new Square("black", 2);
+        Rectangle r = new Rectangle("Red", 2, 4);
+        Circle c = new Circle("Yellow", 3);
+
+
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach(Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+            static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int sqr = number * number;
+            
+            return sqr;
+        }
+        static void DisplayResult(string name, int sqr)
+        {
+            Console.WriteLine($"{name}, the square of your number is {sqr}.");
+        }
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
